86db90fedee6db09d5a38830b527e738a3f8f4d9
Whitfield <whitryan@amazon.com>
2018-09-19 17:32:52 -0700

    Updates

---
+++ precache-manifest.25f319ca935cd76c61a70878607390a6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1a9c00e81a1d02544d5",
+    "url": "/ripe/static/js/runtime~main.d1a9c00e.js"
+  },
+  {
+    "revision": "5a69c7e9c75aa2d0e684",
+    "url": "/ripe/static/js/main.5a69c7e9.chunk.js"
+  },
+  {
+    "revision": "edaf5e02d65c35e49e1b",
+    "url": "/ripe/static/js/1.edaf5e02.chunk.js"
+  },
+  {
+    "revision": "5a69c7e9c75aa2d0e684",
+    "url": "/ripe/static/css/main.2d10dcc5.chunk.css"
+  },
+  {
+    "revision": "a16d0aee85b7fb7b19ecbaaae9c7e0a6",
+    "url": "/ripe/index.html"
+  }
+];
